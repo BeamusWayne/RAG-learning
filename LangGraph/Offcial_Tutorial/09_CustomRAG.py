71f9c77a5df60e3ed8fa51b38699d6dbeb84a76a
Geralt2026 <845326741@qq.com>
2026-03-06 10:14:59 +0800

    LangGraph Official Tutorial Custom RAG and Orchestrator

---
+++ LangGraph/Offcial_Tutorial/09_CustomRAG.py
@@ -0,0 +1,50 @@
+import getpass
+import os
+
+
+def _set_env(key: str):
+    if key not in os.environ:
+        os.environ[key] = getpass.getpass(f"{key}:")
+
+
+_set_env("DASHSCOPE_API_KEY")
+
+
+from langchain_community.document_loaders import WebBaseLoader
+
+urls = [
+    "https://lilianweng.github.io/posts/2024-11-28-reward-hacking/",
+    "https://lilianweng.github.io/posts/2024-07-07-hallucination/",
+    "https://lilianweng.github.io/posts/2024-04-12-diffusion-video/",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+
+docs[0][0].page_content.strip()[:1000]
+
+
+
+
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_openai import OpenAIEmbeddings
+
+vectorstore = InMemoryVectorStore.from_documents(
+    documents=doc_splits, embedding=OpenAIEmbeddings()
+)
+retriever = vectorstore.as_retriever()
+
+
+
+
+from langchain.tools import tool
+
+@tool
+def retrieve_blog_posts(query: str) -> str:
+    """Search and return information about Lilian Weng blog posts."""
+    docs = retriever.invoke(query)
+    return "\n\n".join([doc.page_content for doc in docs])
+
+retriever_tool = retrieve_blog_posts
+
+
+retriever_tool.invoke({"query": "types of reward hacking"})
